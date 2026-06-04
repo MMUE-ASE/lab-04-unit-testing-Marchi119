3a48b86b9d86f1bbe84f287657338d9a061b1feb
Ruben Diaz <rubendzcas@gmail.com>
2026-06-04 17:09:41 +0200

    test(lab4): resolver ejercicio 1 de tests basicos con Unity

--- docs/exercises/test/test_calculator.c
+++ docs/exercises/test/test_calculator.c
@@ -38,8 +38,8 @@ void test_calc_add_two_positive_numbers(void)
  */
 void test_calc_add_cancels_to_zero(void)
 {
-    /* YOUR CODE HERE */
-    TEST_IGNORE_MESSAGE("Remove this line and write the test");
+    int result = calc_add(-4, 4);
+    TEST_ASSERT_EQUAL_INT(0, result);
 }
 
 /* ----- TODO EX1.2 ------------------------------------------------------- *
@@ -47,8 +47,9 @@ void test_calc_add_cancels_to_zero(void)
  */
 void test_calc_subtract_basic(void)
 {
-    /* YOUR CODE HERE */
-    TEST_IGNORE_MESSAGE("Remove this line and write the test");
+    
+    int result = calc_subtract(10, 3);
+    TEST_ASSERT_EQUAL_INT(7, result);
 }
 
 /* ----- TODO EX1.3 ------------------------------------------------------- *
@@ -57,6 +58,6 @@ void test_calc_subtract_basic(void)
  */
 void test_calc_multiply_negative(void)
 {
-    /* YOUR CODE HERE */
-    TEST_IGNORE_MESSAGE("Remove this line and write the test");
+    int result = calc_multiply(-6, 7);
+    TEST_ASSERT_EQUAL_INT(-42, result);
 }
